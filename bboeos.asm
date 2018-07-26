a956799afabb063af0db6dbcf29e54c56761cadb
Bryce Boe <bbzbryce@gmail.com>
2018-07-26 10:55:49 -0700

    Improve formatting

--- bboeos.asm
+++ bboeos.asm
@@ -1,4 +1,4 @@
-	BITS 16
+        BITS 16
 
 start:
         mov ax, 07C0h           ; Set data segment to where we're loaded
@@ -27,19 +27,19 @@ start:
         version_string db 'Version 0.0.2dev', 0
         another_string db '...', 0
 
-print_string:			; Routine: output string in SI to screen
-	mov ah, 0Eh		; int 10h 'print char' function
+print_string:                   ; Routine: output string in SI to screen
+        mov ah, 0Eh             ; int 10h 'print char' function
 
-	.repeat:
-	lodsb			; Get character from string
-	cmp al, 0
-	je .done		; If char is zero, end of string
-	int 10h			; Otherwise, print it
-	jmp .repeat
+        .repeat:
+        lodsb                   ; Get character from string
+        cmp al, 0
+        je .done                ; If char is zero, end of string
+        int 10h                 ; Otherwise, print it
+        jmp .repeat
 
-	.done:
-	ret
+        .done:
+        ret
 
 
-	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
-	dw 0xAA55		; The standard PC boot signature
+        times 510-($-$$) db 0   ; Pad remainder of boot sector with 0s
+        dw 0AA55h               ; The standard PC boot signature
