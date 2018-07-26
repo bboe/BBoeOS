99f9894d38f5627fbaed692ef24d963adae00712
Bryce Boe <bbzbryce@gmail.com>
2018-07-26 10:53:50 -0700

    Don't move stack space at this time

--- bboeos.asm
+++ bboeos.asm
@@ -1,11 +1,8 @@
 	BITS 16
 
 start:
-	mov ax, 0x8e0		; Set up 4K stack space after this bootloader
-	mov ss, ax		; (4096 + 512) / 16 bytes per paragraph
-	mov sp, 4096
-	mov ax, 0x07C0		; Set data segment to where we're loaded
-	mov ds, ax
+        mov ax, 07C0h           ; Set data segment to where we're loaded
+        mov ds, ax
 
 
 	mov si, welcome_string	; Put string position into SI
