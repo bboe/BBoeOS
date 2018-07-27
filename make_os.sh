1e2a9958b87f3c53fd78789b0c7d088d6d98c76d
Bryce Boe <bbzbryce@gmail.com>
2018-07-26 22:39:54 -0700

    Output bin to os.bin

--- make_os.sh
+++ make_os.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-nasm -f bin -o bboeos.bin bboeos.asm
+nasm -f bin -o os.bin bboeos.asm
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
 dd bs=512 count=2880 if=/dev/zero of=floppy.img
-dd conv=notrunc if=bboeos.bin of=floppy.img
+dd conv=notrunc if=os.bin of=floppy.img
 #rm bboeos.bin
