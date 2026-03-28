824233c0eaa273195f874ae684fcea11b32289c9
Bryce Boe <bbzbryce@gmail.com>
2026-03-27 21:37:52 -0700

    Remove dedicated clear command

--- src/programs/shell.asm
+++ src/programs/shell.asm
@@ -67,10 +67,6 @@ main:
 ;;; Command handlers
 ;;; Return: SI = string to print, or SI = 0 for no output
 
-cmd_clear:
-        mov ah, SYS_SCR_CLEAR
-        jmp syscall_null
-
 cmd_help:
         push bx
         mov si, HELP_PREFIX
@@ -139,13 +135,11 @@ syscall_null:
 
 ;;; Command table
 cmd_table:
-        dw .clear,    cmd_clear
         dw .help,     cmd_help
         dw .ls,       cmd_ls
         dw .reboot,   cmd_reboot
         dw .shutdown, cmd_shutdown
         dw 0
-        .clear    db `clear\0`
         .help     db `help\0`
         .ls       db `ls\0`
         .reboot   db `reboot\0`
